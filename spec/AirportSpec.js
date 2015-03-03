67a46625739b7c04eab846e47c4f76582c42363a
Kevin Lanzon <kevinlanzon@gmail.com>
2015-03-03 19:26:03 +0000

    refactored

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,16 +1,26 @@
 describe ('Airport', function() {
   var airport;
 
-  it('should have no planes when created', function() {
+  beforeEach(function() {
     airport = new Airport();
+    plane = new Plane();
+  });
+
+  it('should have no planes when created', function() {
     expect(airport.planes).toEqual([]);
   });
 
   it('should be able to accept a plane', function() {
-    plane = new Plane();
     plane.land();
     airport.park(plane);
     expect(airport.planes).toEqual([plane]);
+  });
+
+  it('should be able to release a plane', function() {
+    plane.land();
+    airport.park(plane);
+    airport.takeOff(plane);
+    expect(airport.planes).toEqual([]);
   });
 
 });
--- src/Airport.js
+++ src/Airport.js
@@ -9,3 +9,7 @@ Airport.prototype.planes = function() {
 Airport.prototype.park = function(plane) {
   this.planes.push(plane)
 };
+
+Airport.prototype.takeOff = function(plane) {
+  this.planes.splice(plane)
+};
